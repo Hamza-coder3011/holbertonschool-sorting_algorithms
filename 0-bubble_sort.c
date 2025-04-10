c136be5de9750aef3f1e1be90f1594995f71931b
Hamza-coder3011 <10606@holbertonstudents.com>
2025-04-10 22:09:09 +0200

    update file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,14 +10,12 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int swapped;
 
 	if (!array || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 
 		for (j = 0; j < size - 1 - i; j++)
 		{
@@ -26,12 +24,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
 				print_array(array, size);
 			}
 		}
-
-		if (!swapped)
-			break;
 	}
 }
--- sort.h
+++ sort.h
@@ -2,8 +2,6 @@
 #define SORT_H
 
 #include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
 * struct listint_s - Doubly linked list node
@@ -26,8 +24,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-void quick_sort_helper(int *array, int low, int high, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
 
 #endif
