2b531228fa822b51cd1d7bc4c6bbd2ae44717cb0
Hamza-coder3011 <10606@holbertonstudents.com>
2025-04-10 22:13:25 +0200

    update files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int tmp
 
 	if (!array || size < 2)
 		return;
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
--- sort.h
+++ sort.h
@@ -7,8 +7,8 @@
 * struct listint_s - Doubly linked list node
 *
 * @n: Integer stored in the node
-* @prev: Pointer to the previous element of the list
-* @next: Pointer to the next element of the list
+* @prev: Pointer to the previous element
+* @next: Pointer to the next element
 */
 typedef struct listint_s
 {
